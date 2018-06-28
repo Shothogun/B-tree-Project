a6b129260d60603c96449e12280287cf5c5e2137
Otho Komatsu <otho.tk@hotmail.com>
2018-06-28 18:13:59 -0300

    Primary_key creator consertado

--- include/B_Tree.hpp
+++ include/B_Tree.hpp
@@ -8,24 +8,30 @@ using namespace std;
 
 typedef struct primary_key
 {
-	string primary_key_value;
-	string NRR;
+	std::string primary_key_value;
+	std::string NRR;
 
 } primary_key;
 
 class node_B_Tree
 {
 	private:
-		std::vector<primary_key> primary_key_vector;
-		std::vector<int> pointer;
+		int counter;
+		int line_number;
 
 	public:
-		int index_file_creator();
-		void insert_data();
+		std::vector<primary_key> primary_key_vector;
+		bool leaf;
+		std::vector<int> pointer;
+		int index_file_creator(std::string name_in);
+		void insert_data(std::string primary_key_input, std::string NRR_input);
+		void insert_data_non_full(std::string k, std::string NRR_input);
+		void split_child();
 		void delete_data();
 		void search_data();
 };
 
 string primary_key_creator(string line, string line_ws);
+bool compare_primary_key(const primary_key &a, const primary_key &b);
 
 #endif
